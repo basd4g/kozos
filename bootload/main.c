44fb100a7dc77ce2fa5184dedef36516140f228c
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-11-05 02:37:11 +0900

    STEP3.2 静的変数の書き換えに対応

--- bootload/main.c
+++ bootload/main.c
@@ -2,16 +2,60 @@
 #include "serial.h"
 #include "lib.h"
 
+int global_data = 0x10;
+int global_bss;
+static int static_data = 0x20;
+static int static_bss;
+
+static int init(void)
+{
+	/* 以下はリンカ・スクリプトで定義してあるシンボル */
+	extern int erodata, data_start, edata, bss_start, ebss; // 各シンボルを参照可能にする
+
+	/* データ領域とBSS領域を初期化する。この処理以降でないとグローバル変数が初期化されていないので注意 */
+	memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+	memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+	// シリアルの初期化
+	serial_init(SERIAL_DEFAULT_DEVICE);
+
+	return 0;
+}
+
+static void printval(void)
+{
+	puts("global_data = ");
+	putxval(global_data, 0);
+	puts("\n");
+
+	puts("global_bss = ");
+	putxval(global_bss, 0);
+	puts("\n");
+
+	puts("static_data = ");
+	putxval(static_data, 0);
+	puts("\n");
+
+	puts("static_bss = ");
+	putxval(static_bss, 0);
+	puts("\n");
+}
+
 int main(void)
 {
-	serial_init(SERIAL_DEFAULT_DEVICE);
+	init();
+
 	puts("hello,world!\n");
 
-	putxval(0x10, 0);
-	puts("\n");
+	printval();
 
-	putxval(0xffff, 0);
-	puts("\n");
+	puts("overwrite variables. \n");
+	global_data = 0x20;
+	global_bss = 0x30;
+	static_data = 0x40;
+	static_bss = 0x50;
+
+	printval();
 
 	while(1)
 		;
